26c5fe9a1b71c901986f806a1b380d3769149337
Ethan Kent <ekent@jd11.law.harvard.edu>
2016-07-19 16:59:07 -0500

    Removed uneccessary sort.

--- src/knapsack/knapsack.ts
+++ src/knapsack/knapsack.ts
@@ -15,7 +15,7 @@ export class Knapsack {
   getResults() { return this.results; }
 
   private hashArgs(prices, budget) {
-    return hash(hash(prices.toJS().sort()) + hash(budget));
+    return hash(hash(prices.toJS()) + hash(budget));
   }
 
   private computeHelper(
